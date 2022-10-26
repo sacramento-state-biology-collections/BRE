a227823bc64fc65de06b4f2c50dc5ca5988d9f3b
Almaguer, Brian <balmaguer@csus.edu>
2022-10-26 16:23:00 -0700

    Connected ui to javascript, changes made

--- javascript/templates/search-bar.js
+++ javascript/templates/search-bar.js
@@ -1,23 +1,31 @@
 Vue.component("search-bar", {
   template: `
-  <header>
-    <h1>{{title}}</h1>
-    <button class="home">home</button>
-    <input type="text" placeholder="search collection" v-model="searchQuery"></header>
-    <select name="collections" id="selection">
-        <option v-for="option in options" :value="option">{{option}}</option>
-    </select>
-    <button class="search" @click="search"></button>
-  </header>     
+    <header>
+        <h1>{{title}}</h1>
+        <button class="home">home</button>
+        <input type="text" v-model="searchText" />
+        <select name="Collections" id="Collections" v-on:change="update">
+            <option v-for="option in options" v-bind:value="option">{{option}}</option>
+        </select>
+        <button class="search" v-on:click="search">search</button>
+    </header>     
     `,
   data: function () {
     return {
-      search: "",
+      title: "My Collection",
+      options: ["All", "Mammals", "Birds", "Reptiles", "Amphibians", "Fish"],
+      searchText: "Search Collection",
+      collection: "",
     };
   },
   methods: {
+    update: function (event) {
+      this.collection = event.target.value;
+    },
     search: function () {
-      this.$emit("search", this.search);
+      console.log(
+        "searching for " + this.searchText + " in " + this.collection
+      );
     },
   },
 });
