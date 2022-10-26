d0b39a7b6c00710ba6e2e43896c1dafdb23f5e6a
May, Joseph <josephmay@csus.edu>
2022-10-26 15:35:33 -0700

    added search bar html

--- javascript/templates/search-bar.js
+++ javascript/templates/search-bar.js
@@ -1,9 +1,14 @@
 Vue.component("search-bar", {
   template: `
-    <div class="search-bar">
-        <input type="text" placeholder="Search" v-model="searchQuery" />
-        <button @click="search">Search</button>
-    </div>
+  <header>
+    <h1>{{title}}</h1>
+    <button class="home">home</button>
+    <input type="text" placeholder="search collection" v-model="searchQuery"></header>
+    <select name="collections" id="selection">
+        <option v-for="option in options" :value="option">{{option}}</option>
+    </select>
+    <button class="search" @click="search"></button>
+  </header>     
     `,
   data: function () {
     return {
