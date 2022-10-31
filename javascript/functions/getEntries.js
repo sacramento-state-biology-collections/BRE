62f0172be0e146320b1a999a072f5e3fc80312fe
Almaguer, Brian <balmaguer@csus.edu>
2022-10-31 15:50:21 -0700

    semi working

--- javascript/functions/getEntries.js
+++ javascript/functions/getEntries.js
@@ -1,10 +1,22 @@
-function getEntries() {
-    Papa.parse("./data/mammals-collection.csv", {
-        header: true,
-        download: true,
-        dynamicTyping: true,
-        complete: function(results) {
-          data = results.data;
-        }
-    });
+Papa.parse("./data/mammals-collection.csv", {
+    header: true,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+      data = results.data;
+    }
+});
+
+async function getEntries() {
+    let entries = {};
+    for (let i = 0; i < data.length; i++) {
+        entries[i] = {
+            "Catalog .": data[i]["Catalog ."],
+            "Common Name": data[i]["Common Name"],
+            "Scientific Name": data[i]["Scientific Name"],
+            "Prep Type": data[i]["Prep Type"],
+            "Drawer .": data[i]["Drawer ."],
+        };
+    }
+    return entries;
 }
