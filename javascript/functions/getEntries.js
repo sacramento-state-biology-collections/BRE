1c62b6a1e67db1d066889a35ec9999954d8e8f9f
Almaguer, Brian <balmaguer@csus.edu>
2022-11-07 13:08:44 -0800

    renamed 2 funcs to one and change papaparse import

--- javascript/functions/getEntries.js
+++ javascript/functions/getEntries.js
@@ -50,6 +50,42 @@ request.onsuccess = function () {
         Scientific_Name: data[i]["Scientific Name"],
         Prep_Type: data[i]["Prep Type"],
         Drawer: data[i]["Drawer ."],
+        Age: data[i]["Age"],
+        Available: data[i]["Available?"],
+        Body_Width: data[i]["Body Width"],
+        Cabinet: data[i]["Cabinet ."],
+        City: data[i]["City"],
+        Collection_Circumstance: data[i]["Collection Circumstance"],
+        Collection_Date: data[i]["Collection Date"],
+        Collector_Address: data[i]["Collectors Address"],
+        Collector_Name: data[i]["Collectors Name"],
+        Comments: data[i]["Comments"],
+        Condition: data[i]["Condition"],
+        Country: data[i]["Country"],
+        County: data[i]["County"],
+        Ear_Notch: data[i]["Ear Notch"],
+        Ear_Tag_Leg_band: data[i]["Ear Tag/Leg band ."],
+        Exact_Locality: data[i]["Exact Locality"],
+        Exchange: data[i]["Exchange"],
+        Family: data[i]["Family"],
+        Forearm: data[i]["Forearm"],
+        HMB_107_Bone_Room: data[i]["HMB 107 Bone Room"],
+        HMB_107_Cabinets: data[i]["HMB 107 Cabinets"],
+        HMB_107_Skin_Room: data[i]["HMB 107 Skin Room"],
+        HMB_124: data[i]["HMB 124"],
+        Hind_Foot: data[i]["Hind Foot"],
+        Last_Inventoried: data[i]["Last Inventoried"],
+        Location: data[i]["Locations"],
+        Order: data[i]["Order"],
+        Prep_Date: data[i]["Prep Date"],
+        Preparator: data[i]["Preparator"],
+        SQU105_Display: data[i]["SQU105 Display"],
+        SQU_105_Teaching: data[i]["SQU 105 Teaching"],
+        Sex: data[i]["Sex"],
+        State: data[i]["State"],
+        Tail_Length: data[i]["Tail Length"],
+        Total_Length: data[i]["Total Length"],
+        Weight: data[i]["Weight"],
       });
     } catch (error) {}
   }
@@ -62,14 +98,7 @@ function getSearch() {
   return getEntries;
 }
 
-function getPreview(Catalog) {
-  const transaction = db.transaction("mammals", "readonly");
-  const store = transaction.objectStore("mammals");
-  const getEntry = store.get(Catalog);
-  return getEntry;
-}
-
-function getView(Catalog) {
+function getEntry(Catalog) {
   const transaction = db.transaction("mammals", "readonly");
   const store = transaction.objectStore("mammals");
   const getEntry = store.get(Catalog);
