eff55a847ec0cec0b30d8c2a6e2bd909f155f199
Evan_Boswell <Evanckboswell@gmail.com>
2022-11-14 05:01:09 -0800

    I believe that I have create a way top maintain local storage

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
+//Authors: Brian A. and Evan B.
+//Subtasks: GG-41, GG-27
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+
+// register route for css, js and csv files
+workbox.routing.registerRoute(
+    /\.(?:css|js|csv)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'assets',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 30 * 24 * 60 * 60
+            })
+        ]
+    })
+)
 
 // register route for images
 workbox.routing.registerRoute(
