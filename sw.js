59140e0404354644af85313429de73dce09cfebd
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2022-11-20 23:41:30 -0800

    GG-25 Merge to complete user story (#6)
    * Move funcations and remove empty file
    
    * got the service worker to cache css and js files
    
    * new workbox connection
    
    * GG-28 Add event listener to store cache
    
    * Update Brian-and-Evan Branch (#5)
    
    * changed drawer to room
    
    * gg-59 added room to preview page
    
    Co-authored-by: Bringino, Kelly <kellybringino@csus.edu>
    Co-authored-by: L Prather <63510911+lprather@users.noreply.github.com>
    
    Co-authored-by: Evan_Boswell <Evanckboswell@gmail.com>
    Co-authored-by: Bringino, Kelly <kellybringino@csus.edu>
    Co-authored-by: L Prather <63510911+lprather@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,10 +1,37 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+//Authors: Brian A. and Evan B.
+//Subtasks: GG-41, GG-27
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
+
+// Fallback Page
+// setDefaultHandler(
+//   new NetworkOnly()
+// );
+// offlineFallback();
+
+const SW_VERSION = "1.0.0";
+
+self.addEventListener("message", (event) => {
+  if (event.data.type === "GET_VERSION") {
+    event.ports[0].postMessage(SW_VERSION);
+  }
+});
+
+//redister route for CSS & JS files
+workbox.routing.registerRoute(
+  ({ request }) =>
+    request.destination === "style" || request.destination === "script",
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "static-assets",
+  })
+);
 
 // register route for images
 workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
+  ({ request }) => request.destination === "image",
   new workbox.strategies.CacheFirst({
-    cacheName: 'images',
+    cacheName: "images",
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 50,
@@ -13,3 +40,29 @@ workbox.routing.registerRoute(
     ],
   })
 );
+
+// register a event listener for fetch
+self.addEventListener("fetch", (event) => {
+  // check if event is a api call
+  if (event.request.url.includes("/api/v1/")) {
+    // Cache First, fallback to network
+    event.respondWith(
+      caches.open("api-cache").then((cache) => {
+        // return cache if available
+        return cache.match(event.request).then((response) => {
+          // return response if available
+          if (response) {
+            return response;
+          }
+          // fetch from network
+          return fetch(event.request).then((response) => {
+            // cache response
+            cache.put(event.request, response.clone());
+            // return response
+            return response;
+          });
+        });
+      })
+    );
+  }
+});
